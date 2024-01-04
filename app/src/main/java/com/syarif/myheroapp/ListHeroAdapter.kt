670e24f03f7c1299fd9933c038b55bdf1ec98246
achmadsyarif252 <19kaizen@gmail.com>
2024-01-04 21:55:54 +0700

    add glide libraru

--- app/src/main/java/com/syarif/myheroapp/ListHeroAdapter.kt
+++ app/src/main/java/com/syarif/myheroapp/ListHeroAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class ListHeroAdapter(private val listHero: ArrayList<Hero>) :
     RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
@@ -31,7 +32,7 @@ class ListHeroAdapter(private val listHero: ArrayList<Hero>) :
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         val (name, desription, photo) = listHero[position]
-        holder.imgPhoto.setImageResource(photo)
+        Glide.with(holder.itemView.context).load(photo).into(holder.imgPhoto)
         holder.tvName.text = name
         holder.tvDescription.text = desription
         holder.itemView.setOnClickListener {
